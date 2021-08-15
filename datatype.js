ead1a15064c6d147b56e695de4d4ae5b252c8682
SamSmith21 <sammy_smith_@hotmail.com>
2021-08-15 16:35:59 +0100

    Update css and js

--- datatype.js
+++ datatype.js
@@ -28,9 +28,23 @@ dateNew.innerHTML = `${day}, ${hour}:${minute}`;
 ///////////////////
 
 function showTemperature(response) {
+  let descriptionElement = document.querySelector("#description");
   let temperature = Math.round(response.data.main.temp);
   let tempNew = document.querySelector("#tempNew");
-  tempNew.innerHTML = `${temperature}°C`;
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = Math.round(response.data.main.temp);
+
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  tempNew.innerHTML = `${celsiusTemperature}°C`;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function enterCity(event) {
@@ -54,9 +68,8 @@ form.addEventListener("submit", enterCity);
 /////////////////////
 
 function showButtonTemp(response) {
-  let temperature = Math.round(response.data.main.temp);
   let tempNew = document.querySelector("#tempNew");
-  tempNew.innerHTML = `${temperature}°C`;
+  tempNew.innerHTML = `${celsiusTemperature}°C`;
 }
 
 function showPosition(position) {
@@ -84,8 +97,12 @@ clickButtone.addEventListener("click", clickButton);
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#tempNew");
+  celsiuslink.classList.remove("active");
+  fahrenheitlink.classList.add("active");
   let temperature = temperatureElement.innerHTML;
-  temperatureElement.innerHTML = `${Math.round((temperature * 9) / 5 + 32)}°F`;
+  temperatureElement.innerHTML = `${Math.round(
+    (celsiusTemperature * 9) / 5 + 32
+  )}`;
 }
 
 let fahrenheitlink = document.querySelector("#fahrenheit-link");
@@ -94,9 +111,12 @@ fahrenheitlink.addEventListener("click", convertToFahrenheit);
 function convertToCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#tempNew");
-  let temperatureCelsius = temperatureElement.innerHTML;
-  temperatureElement.innerHTML = `${temperatureCelsius}°C`;
+  celsiuslink.classList.add("active");
+  fahrenheitlink.classList.remove("active");
+  temperatureElement.innerHTML = `${celsiusTemperature}`;
 }
 
 let celsiuslink = document.querySelector("#celsius-link");
 celsiuslink.addEventListener("click", convertToCelsius);
+
+let celsiusTemperature = null;
